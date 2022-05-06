0666c7e96b53786ea6159dcb476601f96048a4bc
tht2 <quocvietk5959@gmail.com>
2022-05-06 13:41:54 +0700

    update

---
+++ VietShop/Model/categoryModel.php
@@ -0,0 +1,58 @@
+<?php
+include './Config/ConnectDB.php';
+class CategoryModel{
+
+    public static function getAll()
+    {
+        global $conn;
+        $sql = "SELECT * FROM category";
+        $stmt = $conn->query($sql);
+        //Thiet lap csdl tra ve
+        $stmt->setFetchMode(PDO::FETCH_OBJ);
+        //fetchALL se tra ve du lieu nhieu hon 1 ket qua
+        $rows = $stmt->fetchAll();
+        //Tra du lieu ve controller
+        return $rows;
+    }
+    public static function search_name($key)
+    {
+        global $conn;
+        $sql = "SELECT * FROM `category` WHERE name like'%$key%'";
+
+        $stmt = $conn->query($sql);
+        //Thiet lap csdl tra ve
+        $stmt->setFetchMode(PDO::FETCH_OBJ);
+        //fetchALL se tra ve du lieu nhieu hon 1 ket qua
+        $rows = $stmt->fetchAll();
+        //Tra du lieu ve controller
+        return $rows;
+    }
+    public static function find($id)
+    {
+        global $conn;
+        $sql = "SELECT * FROM `category` WHERE `id` = $id";
+        $stmt = $conn->query($sql);
+        $stmt->setFetchMode(PDO::FETCH_OBJ);
+        //fetch se tra ve du lieu 1 ket qua
+        $row = $stmt->fetch();
+        return $row;
+    }
+    public static function create($name)
+    {
+        global $conn;
+        $sql = "INSERT INTO category  (name) VALUES ('$name')";
+        $conn->query($sql); 
+    }
+    public static function update($id, $name)
+    {
+        global $conn;
+        $sql = "UPDATE category SET name = '$name'WHERE id = '$id'";
+        $conn->query($sql);
+
+    }
+    public static function delete($id){
+        global $conn;
+        $sql = "DELETE FROM category WHERE id = '$id'";
+        $conn->query($sql);
+    }
+}
---
+++ VietShop/Model/productModel.php
@@ -0,0 +1,65 @@
+<?php
+include './Config/ConnectDB.php';
+class ProductModel
+{
+    public static function getAll()
+    {
+        global $conn;
+        $sql = "SELECT * FROM product  ";
+        $stmt = $conn->query($sql);
+        //Thiet lap csdl tra ve
+        $stmt->setFetchMode(PDO::FETCH_OBJ);
+        //fetchALL se tra ve du lieu nhieu hon 1 ket qua
+        $rows = $stmt->fetchAll();
+        //Tra du lieu ve controller
+        return $rows;
+    }
+
+    public static function search($key)
+    {
+        global $conn;
+        $sql = "SELECT * FROM `product` WHERE masp like'%$key%';";
+
+        $stmt = $conn->query($sql);
+        //Thiet lap csdl tra ve
+        $stmt->setFetchMode(PDO::FETCH_OBJ);
+        //fetchALL se tra ve du lieu nhieu hon 1 ket qua
+        $rows = $stmt->fetchAll();
+        //Tra du lieu ve controller
+        return $rows;
+    }
+    public static function find($id)
+    {
+        global $conn;
+        $sql = "SELECT * FROM `product` WHERE `id` = $id";
+        $stmt = $conn->query($sql);
+        $stmt->setFetchMode(PDO::FETCH_OBJ);
+        //fetch se tra ve du lieu 1 ket qua
+        $row = $stmt->fetch();
+        return $row;
+    }
+    public static function create($data)
+    {
+        global $conn;
+        $title = $data['title'];
+        $description = $data['description'];
+        $image = $data['image'];
+        $quantity = $data['quantity'];
+        $price = $data['price'];
+        $category = $data['category_id'];
+        $sql = "INSERT INTO product  (category_id,title,description,image,quantity,price) VALUES ('$category','$title', '$description', '$image', '$quantity',' $price')";
+        $conn->query($sql);
+    }
+    public static function update($id, $title, $description, $image, $quantity, $price)
+    {
+        global $conn;
+        $sql = "UPDATE product SET title = '$title',description = '$description',image = '$image',quantity = '$quantity',price = '$price' WHERE id = '$id'";
+        $conn->query($sql);
+    }
+    public static function delete($id)
+    {
+        global $conn;
+        $sql = "DELETE FROM product WHERE id = '$id'";
+        $conn->query($sql);
+    }
+}
